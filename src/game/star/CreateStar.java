eccf1e509ab51b81d6f38f6db4f908d5cc027ab3
vukien <vukien104@gmail.com>
2018-08-18 20:37:06 +0700

    use action

--- src/game/star/CreateStar.java
+++ src/game/star/CreateStar.java
@@ -1,5 +1,6 @@
 package game.star;
 
+import action.*;
 import base.FrameCounter;
 import base.GameObject;
 import base.GameObjectManager;
@@ -9,21 +10,43 @@ import java.util.Random;
 public class CreateStar extends GameObject {
 
     private Random random;
-    private FrameCounter frameCounter;
+//    private FrameCounter frameCounter;
 
     public CreateStar() {
         this.random = new Random();
-        this.frameCounter = new FrameCounter(30);
+//        this.frameCounter = new FrameCounter(30);
+        this.configAction();
     }
 
-    @Override
-    public void run() {
-        super.run();
-        if (this.frameCounter.run()) {
-            Star star = GameObjectManager.instance.recycle(Star.class);
-            star.position.set(1024, this.random.nextInt(600));
-            star.velocity.set(this.random.nextInt(3) + 1, 0);
-            this.frameCounter.reset();
-        }
+    public void configAction() {
+        // Cach 1:
+        Action waitAction = new WaitAction(30);
+
+        Action createAction = new ActionAdapter() {
+            @Override
+            public boolean run(GameObject owner) {
+                Star star = GameObjectManager.instance.recycle(Star.class);
+                star.position.set(1024, random.nextInt(600));
+                star.velocity.set(random.nextInt(3) + 1, 0);
+                return true;
+            }
+        };
+
+        Action sequenceAction = new SequenceAction(waitAction, createAction);
+
+        Action repeatAction = new RepeatActionForever(sequenceAction);
+
+        this.addAction(repeatAction);
     }
+
+//    @Override
+//    public void run() {
+//        super.run();
+//        if (this.frameCounter.run()) {
+//            Star star = GameObjectManager.instance.recycle(Star.class);
+//            star.position.set(1024, this.random.nextInt(600));
+//            star.velocity.set(this.random.nextInt(3) + 1, 0);
+//            this.frameCounter.reset();
+//        }
+//    }
 }
