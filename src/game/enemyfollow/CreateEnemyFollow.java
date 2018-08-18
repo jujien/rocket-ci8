722c8654fcbeea28445dbf859b1577ac5bac2774
vukien <vukien104@gmail.com>
2018-08-18 21:14:33 +0700

    add limit action && action create star, enemyfollow

--- src/game/enemyfollow/CreateEnemyFollow.java
+++ src/game/enemyfollow/CreateEnemyFollow.java
@@ -1,5 +1,9 @@
 package game.enemyfollow;
 
+import action.ActionAdapter;
+import action.LimitAction;
+import action.SequenceAction;
+import action.WaitAction;
 import base.FrameCounter;
 import base.GameObject;
 import base.GameObjectManager;
@@ -11,14 +15,28 @@ public class CreateEnemyFollow extends GameObject {
     private FrameCounter frameCounter = new FrameCounter(400);
     private Random random = new Random();
 
-    @Override
-    public void run() {
-        super.run();
-        if (this.frameCounter.run()) {
-            EnemyFollow enemyFollow = new EnemyFollow();
-            enemyFollow.position.set(this.random.nextInt(1024), this.random.nextInt(600));
-            GameObjectManager.instance.add(enemyFollow);
-            this.frameCounter.reset();
-        }
+    public CreateEnemyFollow() {
+        this.configAction();
     }
+
+    public void configAction() {
+        this.addAction(
+                new LimitAction(
+                        70,
+                        new SequenceAction(
+                                new WaitAction(400),
+                                new ActionAdapter() {
+                                    @Override
+                                    public boolean run(GameObject owner) {
+                                        EnemyFollow enemyFollow = GameObjectManager.instance.recycle(EnemyFollow.class);
+                                        enemyFollow.position.set(random.nextInt(1024), random.nextInt(600));
+                                        return true;
+                                    }
+                                }
+                        )
+                )
+        );
+    }
+
+
 }
